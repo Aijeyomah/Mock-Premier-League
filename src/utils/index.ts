e0ffbc891c9d48adbf1972d6749b8d868b422813
aijey_omah <ijayeti@gmail.com>
2022-03-02 20:53:22 +0100

    feature(fixture): add and fetch fixtures

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,7 +3,7 @@ import genericErrors from './error/generic';
 import ApiError from './error/api.error';
 import ModuleError from './error/module.error';
 import DBError from './error/db.error';
-import Helper from './Helper';
+import Helper from './helper';
 
 
 export {
